1d3196cf2f0bd7eb560c42641d3fd164976c13a7
Sectimus <Sectimus@hotmail.co.uk>
2021-07-05 22:03:29 +0100

    added clause for 0 size arrays

--- challenge.py
+++ challenge.py
@@ -3,8 +3,8 @@ def groupArrayElements(arr, n):
 
     output = []
     # start from 0, go til n*sizeperchunk and increment by sizeperchunk (so that there are no overlaps)
-    # i represents the start index of each chunk
-    for i in range(0, n*sizeperchunk, sizeperchunk):
+    # i represents the start index of each chunk (incremental defaults to 1 if 0 / falsy to account for empty arrays)
+    for i in range(0, n*sizeperchunk, sizeperchunk or 1):
         output.append(arr[i:i+sizeperchunk])
 
     return output
